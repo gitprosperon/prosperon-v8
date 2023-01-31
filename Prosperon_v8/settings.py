f44e78a16c350beb674dc411c710a69f22269b0b
mark-jake <104036351+mark-jake@users.noreply.github.com>
2023-01-31 11:44:48 -0500

    test

--- Prosperon_v8/settings.py
+++ Prosperon_v8/settings.py
@@ -99,14 +99,14 @@ WSGI_APPLICATION = 'Prosperon_v8.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd8dr5loeq07nqc',
-        'USER': 'ufe02ks1pc18v3',
-        'PASSWORD': 'p72dc4291c151e1f99d78b10cdb0f86df51acc836d2af2311223ca8aabd5e46c3',
-        'HOST': 'ec2-54-243-252-102.compute-1.amazonaws.com',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
